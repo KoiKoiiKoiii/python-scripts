9d938c4ea3cc6bede0a3aea8fc3a18925299ad3e
Bryson Schueller <Bryson>
2025-10-22 22:46:18 -0600

    Auto update 2025-10-22 22:46:18

--- PartCalculator.py
+++ PartCalculator.py
@@ -3,9 +3,12 @@
 
 
 # Enter the number of circuit boards to be produced
-circuitBoardsNeeded = 250
+circuitBoardsNeeded = 0
+
+# Current inventory of materials
 currentPolymerCount = 500
 currentPureQuartzCount = 9
+
 # Materials required per circuit board
 polymerPerBoard = 2
 pureQuartzPerBoard = 4
@@ -17,25 +20,60 @@ totalPolymer = circuitBoardsNeeded * polymerPerBoard - currentPolymerCount if ci
 totalPureQuartz = circuitBoardsNeeded * pureQuartzPerBoard - currentPureQuartzCount if circuitBoardsNeeded * pureQuartzPerBoard > currentPureQuartzCount else 0
 totalPalOilCost = polymerPalOilCostPer * totalPolymer if totalPolymer > 0 else 0
 
-
 runsNeeded = totalPureQuartz / pureQuartzPerRun
 
 # Pal Oil cost per unit of Polymer
-# Calculate total Pal Oil cost for Polymer needed 
-
-# 30 35 35 Handiwork High Quality Cloth
-# 30 + 35 + 35 = 100
-
-# Kindling 150 Plasteel
-
+ 
 # Output the results
-print('---')
-print("Total Polymer needed: " + str(totalPolymer))
-print('Total Pal Oil cost for remaining Polymer: ' + str(totalPalOilCost))
-print('---')
-print("Total Pure Quartz needed: " + str(totalPureQuartz))
-print("Runs needed for Pure Quartz: " + str(runsNeeded))
-print('---')
+if circuitBoardsNeeded > 0:
+    print('---')
+    print("Total Polymer needed: " + str(totalPolymer))
+    print('Total Pal Oil cost for remaining Polymer: ' + str(totalPalOilCost))
+    print('---')
+    print("Total Pure Quartz needed: " + str(totalPureQuartz))
+    print("Runs needed for Pure Quartz: " + str(runsNeeded))
+    print('---')
 
 # To run this script, make sure you have Python installed on your system.
 # Save this code in a file named PartCalculator.py and run it using the command: python
+
+
+
+# Legendary Sphere Cost Calculator
+
+legendarySphereLF = 999
+legendarySphereCost = {
+    "Paldium": 10,
+    "Pal Metal": 5,
+    "Carbon Fiber": 3,
+    "Cement": 5
+}
+
+carbonFiberCost = {
+    "Coal": 2
+}
+
+cementCost = {
+    "Stone": 50,
+    "Bone": 1,
+    "Pal Fluid": 1
+}
+
+
+totalCost = {}
+
+
+print('--- Legendary Sphere Total Material Requirements ---')
+print(f'Calculating for {legendarySphereLF} Legendary Spheres:')
+for material, quantity in legendarySphereCost.items():
+    totalCost[material] = quantity * legendarySphereLF
+    print(f'Total {material} needed: {totalCost[material]}')
+
+for material, quantity in carbonFiberCost.items():
+    totalCost[material] = quantity * legendarySphereLF * legendarySphereCost["Carbon Fiber"]
+    print(f'Total {material} needed for Carbon Fiber: {totalCost[material]}')
+
+print ('---')
+for material, quantity in cementCost.items():
+    totalCost[material] = quantity * legendarySphereLF * legendarySphereCost["Cement"]
+    print(f'Total {material} needed for Cement: {totalCost[material]}')
